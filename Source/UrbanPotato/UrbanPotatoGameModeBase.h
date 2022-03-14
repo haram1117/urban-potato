5636a1c5bde86b0721ba19e7c43b3407761d2a19
Haram Park <haram1117@khu.ac.kr>
2022-03-14 23:56:54 +0900

    Project Regenerate

---
+++ Source/UrbanPotato/UrbanPotatoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UrbanPotatoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class URBANPOTATO_API AUrbanPotatoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
