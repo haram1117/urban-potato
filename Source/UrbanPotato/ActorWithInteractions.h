f0b31bdb6bb3a64edaa842fd84e9d176b1603ab2
Haram Park <haram1117@khu.ac.kr>
2022-09-13 23:19:06 +0900

    [feat] 퍼즐 클리어 시 Cinematic 추가

--- Source/UrbanPotato/ActorWithInteractions.h
+++ Source/UrbanPotato/ActorWithInteractions.h
@@ -15,7 +15,15 @@ class URBANPOTATO_API AActorWithInteractions : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AActorWithInteractions();
+	
+	/**
+	 * @brief 액터 overlap 진입 시 켜지는 Widget
+	 */
 	UWidgetComponent* widget;
+	
+	/**
+	 * @brief 해당 퍼즐 클리어 여부
+	 */
 	UPROPERTY(BlueprintReadWrite)
 	bool isClear = false;
 	
@@ -24,19 +32,42 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
 	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
-	
+
+	/**
+	 * @brief PlayerCharacter
+	 */
 	APlayerCharacter* PlayerCharacter;
+	
+	/**
+	 * @brief interaction 위젯 visible 여부 (Actor Overlap 여부)
+	 */
 	UPROPERTY(BlueprintReadWrite)
 	bool isWidgetVisible = false;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	/**
+	 * @brief 각 Actor 마다 implement: 캐릭터와의 상호작용 호출 함수
+	 */
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void Interaction();
+	
+	/**
+	 * @brief 게이지 위젯 비활성화 (버튼 인터랙션 시)
+	 */
 	UFUNCTION(BlueprintImplementableEvent)
 	void GaugeWidgetOff();
+	
+	/**
+	 * @brief ActorWithInteraction 위젯 비활성화 (Actor Overlap X)
+	 */
 	UFUNCTION(BlueprintCallable)
 	void WidgetOff();
+	
+	/**
+	 * @brief ActorWithInteraction 위젯 활성화 (Actor Overlap O)
+	 */
 	UFUNCTION(BlueprintCallable)
 	void WidgetOn();
 
--- Source/UrbanPotato/PlayerCharacter.h
+++ Source/UrbanPotato/PlayerCharacter.h
@@ -139,7 +139,7 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
 	UFUNCTION(BlueprintCallable)
 	void MoveForward(float value);
 
